f9ca5605e266f91181879c5bc930e94c67714282
Scott Conway <scott.conway@robinpowered.com>
2025-02-10 12:09:38 -0500

    removing index js file

--- snake/vite.config.js
+++ snake/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/made-with-ai/snake/'
+  // base: '/made-with-ai/snake/'
 })
