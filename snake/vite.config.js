054b2cba9aef8026ff0bf40ad0576008cc6f1d58
Scott Conway <scott.conway@robinpowered.com>
2025-02-10 16:15:52 -0500

    bundler changes

--- snake/vite.config.js
+++ snake/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/made-with-ai/snake/'
+  base: '/made-with-ai/snake/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 })
