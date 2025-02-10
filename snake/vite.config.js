f36e0e21329df6f4d9157ded78511581aaf0ffcf
Scott Conway <scott.conway@robinpowered.com>
2025-02-10 13:28:16 -0500

    new build files

--- snake/vite.config.js
+++ snake/vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/made-with-ai/snake/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  base: '/made-with-ai/snake/'
 })
