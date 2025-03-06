07be21cd70d4bc62ca9234df819dff51c70ac3a8
Scott Conway <scott.conway@robinpowered.com>
2025-03-06 09:19:25 -0500

    adding build files

--- atoms/vite.config.js
+++ atoms/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/made-with-ai/atoms/',
+  base: '/made-with-ai/atoms/dist/',
   build: {
     outDir: 'dist'
   }
